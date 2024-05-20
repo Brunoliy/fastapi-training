e3084b7a32a80297e191c8de5b9ad318470d1cc9
Bruno de Oliveira <obruno.oliveira@outlook.com>
2024-05-20 12:30:27 -0300

    Schema creation

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_roo_must_return_ok_and_message():
-    client = TestClient(app)
+    client = TestClient(app) #Arrange
 
-    response = client.get('/')
+    response = client.get('/') #Act
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'Message': 'Olá Mundo!'}
